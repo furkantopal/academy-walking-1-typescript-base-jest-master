02467513f9ec18a537c275b7461d45d9546662fd
evgeniisqe <94844572+evgeniisqe@users.noreply.github.com>
2021-11-30 12:12:30 +0100

    mob next [ci-skip] [ci skip] [skip ci]

--- src/main/example.ts
+++ src/main/example.ts
@@ -1,5 +1,12 @@
 export class Example {
   isLeapYear(a: number): boolean {
-    return a % 4 === 0;
+    //return a % 4 === 0
+    if (a % 4 === 0) {
+      if (a % 100 !== 0) {
+        return true;
+      }
+      return false;
+    }
+    return false;
   }
 }
--- src/test/example.test.ts
+++ src/test/example.test.ts
@@ -1,7 +1,11 @@
 import { Example } from "../main/example";
 
 describe("Leap year test", () => {
-  it("should be divisible by 4 ", () => {
+  it("should be divisible by 4", () => {
+    let example: Example = new Example();
+    expect(example.isLeapYear(2000)).toBe(true);
+  });
+  it("shouldn't be divisible by 100", () => {
     let example: Example = new Example();
     expect(example.isLeapYear(2000)).toBe(true);
   });
